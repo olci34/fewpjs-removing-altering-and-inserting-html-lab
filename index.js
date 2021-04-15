775598d7364c7eca91d5038a36ee6a30d827c03c
github username <github email address>
2021-04-14 23:02:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main').remove()
+let body = document.body
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('name','H1')
+newHeader.setAttribute('id','victory')
+newHeader.innerHTML = "Fasharra is the champion"
+body.appendChild(h1)
